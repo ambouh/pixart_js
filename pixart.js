06b266c4b6e6de52cbd2c9f084c88474a2cb8eca
ambouh <aphricanboi@gmail.com>
2017-02-03 13:33:46 -0500

    change color when enter is clicked

--- pixart.js
+++ pixart.js
@@ -6,8 +6,20 @@ var colorField = $('#color-field'),
 setColor.on('click', function (e) {
     //to prevent page from refreshing
     e.preventDefault();
+    changeColor();
+});
+setColor.on('keyup', function (e) {
 
+    if(e.keyCode == 13) {
+        //to prevent page from refreshing
+        e.preventDefault();
+        changeColor();
+    }
+});
+
+
+function changeColor(){
     colorId = colorField.val();
     brush.css('background', colorId);
     console.log("hello!");
-});
+}
