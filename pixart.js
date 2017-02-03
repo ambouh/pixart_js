e6352d75129147ff51c4b8f7eedce1ec056214b2
ambouh <aphricanboi@gmail.com>
2017-02-03 13:19:32 -0500

    change color when button is clicked

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+var colorField = $('#color-field'),
+    setColor = $('#set-color'),
+    brush = $('.brush'),
+    colorId;
 
+setColor.on('click', function (e) {
+    //to prevent page from refreshing
+    e.preventDefault();
+
+    colorId = colorField.val();
+    brush.css('background', colorId);
+    console.log("hello!");
+});
