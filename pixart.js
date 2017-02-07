326f42ef572d6084ee957eb2688c0ddb9f5f6058
ambouh <aphricanboi@gmail.com>
2017-02-07 07:22:58 -0500

    added paint() to paint board and 2 brushes [unused for now]

--- pixart.js
+++ pixart.js
@@ -1,38 +1,76 @@
 var colorField = $('#color-field'),
     setColor = $('#set-color'),
-    brush = $('.brush'),
-    colorId;
+    brush = $('.brush:nth-child(1)'),
+    brush2 = $('.brush:nth-child(2)')
 
-setColor.on('click', function (e) {
-    //to prevent page from refreshing
-    e.preventDefault();
-    changeColor();
-});
+    //,brush3 = $('.brush:nth-child(3)')
+    ;
+
 setColor.on('keyup', function (e) {
 
     if(e.keyCode == 13) {
         //to prevent page from refreshing
         e.preventDefault();
-        changeColor();
+        paint(brush,colorField.val());
     }
 });
 
-makeTwentyDivs();
-
-$('.square').on('mouseover', function () {
-
-    $(this).css('background', colorId);
+setColor.on('click', function (e) {
+    //to prevent page from refreshing
+    e.preventDefault();
+    paint(brush,colorField.val());
 });
 
-function changeColor(){
-    colorId = colorField.val();
-    brush.css('background', colorId);
-}
-
-function makeTwentyDivs() {
+/*makeDivs() - creates 8000 divs that is used as a board*/
+function makeDivs() {
     for(var i=0 ; i<8000 ; i++){
         $('.controls').append("<div class='square'></div>");
     }
 }
+
+makeDivs();
+
+/*paint() - takes two argument a selected element and a color,
+    it applies this color to the element*/
+$('.square').on('mouseover', function () {
+        var selectedElement = $(this);
+        var colorToUse = brush.css('backgroundColor');
+
+        paint(selectedElement, colorToUse);
+    }
+);
+
+function paint(selectedElement, colorToUse){
+    var colorToPaint = colorToUse;
+    selectedElement.css('background', colorToPaint);
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+brush2.on('click', function () {
+    var color2 = brush2.css('backgroundColor');
+    changeColor(color2);
+});
+
+
+
+function changeColor(color){
+    colorId = color;
+    brush.css('background', colorId);
+}
+
+
+*/
+
+
 
 
